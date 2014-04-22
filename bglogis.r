46fd004405728b5654031c6aed259fb0075d6d60
Victor Chung <kindlychung@gmail.com>
2014-04-23 01:14:51 +0200

    simplified away the inner loop in the r script, added bench mark

--- bglogis.r
+++ bglogis.r
@@ -1,53 +1,53 @@
 require(Matrix)
 logreg = function(y, x) {
-	x = as.matrix(x)
-	x = apply(x, 2, scale)
-	x = cbind(1, x)
-	m = nrow(x)
-	n = ncol(x)
-	alpha = 2/m
+    x = as.matrix(x)
+    x = apply(x, 2, scale)
+    x = cbind(1, x)
+    m = nrow(x)
+    n = ncol(x)
+    alpha = 2/m
 
-	# b = matrix(rnorm(n))
-	# b = matrix(summary(lm(y~x))$coef[, 1])
-	b = matrix(rep(0, n))
-	v = exp(-x %*% b)
-	h = 1 / (1 + v)
+    # b = matrix(rnorm(n))
+    # b = matrix(summary(lm(y~x))$coef[, 1])
+    b = matrix(rep(0, n))
+    v = exp(-x %*% b)
+    h = 1 / (1 + v)
 
-	J = -(t(y) %*% log(h) + t(1-y) %*% log(1 -h))
-	derivJ = t(x) %*% (h-y)
+    J = -(t(y) %*% log(h) + t(1-y) %*% log(1 -h))
+    derivJ = t(x) %*% (h-y)
 
 
     derivThresh = 0.0000001
     bThresh = 0.001
-	while(1) {
-		newb = b - alpha * derivJ
-		if(max(abs(b - newb)) < bThresh) {
-			break
-		}
-		v = exp(-x %*% newb)
-		h = 1 / (1 + v)
+    while(1) {
+        newb = b - alpha * derivJ
+        if(max(abs(b - newb)) < bThresh) {
+            break
+        }
+        v = exp(-x %*% newb)
+        h = 1 / (1 + v)
         newderivJ = t(x) %*% (h-y)
         if(max(abs(newderivJ - derivJ)) < derivThresh) {
             break
         }
-		newJ = -(t(y) %*% log(h) + t(0-y) %*% log(1 -h))
+        newJ = -(t(y) %*% log(h) + t(0-y) %*% log(1 -h))
         if(newJ > J) {
             alpha = alpha/2
         }
-		b = newb
-		J = newJ
-		derivJ = newderivJ
-	}
-	w = h^2 * v
-	# # hessian matrix of cost function
-	hess = t(x) %*% Diagonal(x = as.vector(w)) %*% x
-	seMat = sqrt(diag(solve(hess)))
-	zscore = b / seMat
-	cbind(b, zscore)
+        b = newb
+        J = newJ
+        derivJ = newderivJ
+    }
+    w = h^2 * v
+    # # hessian matrix of cost function
+    hess = t(x) %*% Diagonal(x = as.vector(w)) %*% x
+    seMat = sqrt(diag(solve(hess)))
+    zscore = b / seMat
+    cbind(b, zscore)
 }
 
 nr = 5000
-nc = 5
+nc = 2
 # set.seed(17)
 x = matrix(rnorm(nr*nc, 3, 9), nr)
 # x = apply(x, 2, scale)
@@ -59,18 +59,57 @@ y[1:round(nr/2)] = sample(0:1, round(nr/2), repl=T)
 
 ntests = 13
 testglm = function() {
-	for(i in 1:ntests) {
-		res = summary(glm(y~x, family=binomial))$coef[, c(1, 3)]
-	}
-	print(res)
+    nr = 5000
+    nc = 2
+    # set.seed(17)
+    x = matrix(rnorm(nr*nc, 3, 9), nr)
+    # x = apply(x, 2, scale)
+    # y = matrix(sample(0:1, nr, repl=T), nr)
+    h = 1/(1 + exp(-x %*% rnorm(nc)))
+    y = round(h)
+    y[1:round(nr/2)] = sample(0:1, round(nr/2), repl=T)
+    for(i in 1:ntests) {
+        res = summary(glm(y~x, family=binomial))$coef[, c(1, 3)]
+    }
+    res
 }
 
 testlogreg = function() {
-	for(i in 1:ntests) {
-		res = logreg(y, x)
-	}
-	print(res)
+    nr = 5000
+    nc = 2
+    # set.seed(17)
+    x = matrix(rnorm(nr*nc, 3, 9), nr)
+    # x = apply(x, 2, scale)
+    # y = matrix(sample(0:1, nr, repl=T), nr)
+    h = 1/(1 + exp(-x %*% rnorm(nc)))
+    y = round(h)
+    y[1:round(nr/2)] = sample(0:1, round(nr/2), repl=T)
+    for(i in 1:ntests) {
+        res = logreg(y, x)
+    }
+    res
 }
 
 print(system.time(testlogreg()))
 print(system.time(testglm()))
+
+# nbench = 100
+# benchres = matrix(NA, nbench, 2)
+# bothres = matrix(NA, 2*nbench, nc)
+# for(i in 1:nbench) {
+#     x = matrix(rnorm(nr*nc, 3, 9), nr)
+#     h = 1/(1 + exp(-x %*% rnorm(nc)))
+#     y = round(h)
+#     y[1:round(nr/2)] = sample(0:1, round(nr/2), repl=T)
+#     myTime = system.time(myres <- testlogreg())[1]
+#     glmTime = system.time(glmres <- testglm())[1]
+#     timeratio = myTime/glmTime
+#     resdiff = abs(myres[, 2] - glmres[, 2])
+#     resdiff = sum(resdiff[2:length(resdiff)])
+#     benchres[i, ] = c(timeratio, resdiff)
+#     bothres[2*i-1, ] = myres[, 2][2:(nc+1)]
+#     bothres[2*i, ] = glmres[, 2][2:(nc+1)]
+# }
+# head(benchres)
+# head(bothres)
+
--- bglogisc.cpp
+++ bglogisc.cpp
@@ -6,82 +6,82 @@
 #include <cmath>
 
 using namespace boost::math;
+using std::cout;
+using std::endl;
+using arma::max;
+using arma::abs;
+using arma::round;
 
 arma::mat logreg(arma::mat y, arma::mat x) {
-    // arma::mat xstddev = stddev(x);
+    arma::mat xstddev = stddev(x);
     // arma::mat xmean = mean(x);
     // x.each_row() -= xmean;
-    // x.each_row() /= xstddev;
+    x.each_row() /= xstddev;
     // add a col of all ones
     int m = x.n_rows;
     arma::mat allOne(m, 1, arma::fill::ones);
     x.insert_cols(0, allOne);
     int n = x.n_cols;
 
-    double alpha = 1/m;
+    double alpha = 1.0/m;
 
     arma::mat b(n, 1, arma::fill::zeros);
     arma::mat v = exp(-x * b);
     arma::mat h = 1 / (1 + v);
     arma::mat J = -(y.t() * log(h) + (1-y).t() * log(1-h));
-    arma::mat derivJ;
-    derivJ = x.t() * (h-y);
+    arma::mat derivJ = x.t() * (h-y);
 
+    double derivThresh = 0.0000001;
+    double bThresh = 0.001;
     while(1) {
         arma::mat newb = b - alpha * derivJ;
+        if(max(max(abs(b-newb))) < bThresh) break;
+
         v = exp(-x * newb);
         h = 1 / (1 + v);
+
+        arma::mat newderivJ = x.t() * (h-y);
+        if(max(max(newderivJ - derivJ)) < derivThresh) break;
+
         arma::mat newJ = -(y.t() * log(h) + (1-y).t() * log(1-h));
-        // if(newJ(0) >= J(0)) {
-        //     std::cout << "newJ >= J" << std::endl;
-        // } else {
-        //     std::cout << "newJ < J" << std::endl;
-        // }
-        while(newJ(0) >= J(0)) {
-            std::cout << "inner while" << std::endl;
-            alpha /= 1.5;
-            newb = b - alpha * derivJ;
-            v = exp(-x * newb);
-            h = 1 / (1 + v);
-            newJ = -(y.t() * log(h) + (1-y).t() * log(1-h));
-        }
-        if(max(max(abs(b) - newb)) < 0.001) {
-            break;
-        }
+        if(newJ(0) >= J(0)) alpha /= 2.0;
+
         b = newb;
         J = newJ;
-        derivJ = x.t() * (h-y);
+        derivJ = newderivJ;
     }
 
-    arma::mat w = pow(h, 2) * v;
+    arma::mat w = pow(h, 2) % v; //Schur product
     arma::sp_mat wmat(diagmat(w));
     arma::mat hess = x.t() * wmat * x;
-    // arma::mat seMat = sqrt()
-    w.print("w.....");
-    wmat.print("wmat......");
-    hess.print("hess........");
+    arma::mat hessInv = arma::pinv(hess);
+    arma::mat seMat = arma::sqrt(hessInv.diag());
+    arma::mat zscore = b/seMat;
+    arma::mat res = arma::join_rows(b, seMat);
 
-    return J;
+    return res;
 }
 
 
 int main(int argc, char const* argv[])
 {
+    arma::arma_rng::set_seed_random();
+    int nr = 5000;
+    int ncx = 2;
+    arma::mat x(nr, ncx, arma::fill::randn);
+    // arma::mat y = arma::randi<arma::mat>(nr, 1, arma::distr_param(0, 1));
+    arma::mat b(ncx, 1, arma::fill::randn);
+    arma::mat v = exp(-x * b);
+    arma::mat h = 1 / (1 + v);
+    arma::mat y = arma::round(h);
+    y(arma::span(0, nr/2), 0) = arma::randi<arma::mat>((nr/2)+1, 1, arma::distr_param(0, 1));
+
+    int ntests = 13;
+    arma::mat res;
+    for (int i=0; i<ntests; i++)
     {
-        arma::arma_rng::set_seed_random();
-        int nr = 50;
-        int ncx = 2;
-        arma::mat x(nr, ncx, arma::fill::randn);
-        arma::mat y = arma::randi<arma::mat>(nr, 1, arma::distr_param(0, 1));
-        logreg(y, x);
-        // arma::mat xcol;
-        // arma::mat res(ncx, 4);
-        // for(int i=0; i<ncx; i++) {
-        //     xcol = x(arma::span::all, i);
-        //     // res.row(i) = (glmMat(y, xcol, "binomial", "logit")).row(1);
-        //     logreg(y, x);
-        // }
-        // res.print("res..........");
+        res = logreg(y, x);
     }
+    res.print("res........");
     return 0;
 }
